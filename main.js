3dc0bcfb39907187ed6e2afe874790b0e79aa4c0
kamaukimani <paulkimys285@gmail.com>
2025-06-27 11:46:52 +0300

    update

--- main.js
+++ main.js
@@ -3,9 +3,37 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+const modal = document.getElementById("modal");
+const modalMessage = document.getElementById("modal-message");
 
 
+const hearts = document.querySelectorAll(".like-glyph");
+hearts.forEach(heart => {
+  heart.addEventListener("click", () => {
+    mimicServerCall()
+      .then(() => {
+        // If the heart is empty, fill it
+        if (heart.innerText === EMPTY_HEART) {
+          heart.innerText = FULL_HEART;
+          heart.classList.add("activated-heart");
+        } else {
+          // If the heart is full, empty it
+          heart.innerText = EMPTY_HEART;
+          heart.classList.remove("activated-heart");
+        }
+      })
+      .catch(error => {
+        // Show the error modal with the message
+        modal.classList.remove("hidden");
+        modalMessage.innerText = error;
 
+        // Hide the modal after 3 seconds
+        setTimeout(() => {
+          modal.classList.add("hidden");
+        }, 3000);
+      });
+  });
+});
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
